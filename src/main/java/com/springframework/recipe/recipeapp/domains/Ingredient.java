f1aac55d4c50a7bbfd1178c80c73ca1c963bae51
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-12-07 10:42:12 +0100

    -- first commit

---
+++ src/main/java/com/springframework/recipe/recipeapp/domains/Ingredient.java
@@ -0,0 +1,58 @@
+package com.springframework.recipe.recipeapp.domains;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * @author Ehtiram_Abdullayev on 06.12.2018
+ * @project recipe-app
+ */
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+}
